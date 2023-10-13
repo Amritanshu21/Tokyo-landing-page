495a8202af74f0772cb26f671aa014412aed1c01
Amritanshu21 <120729168+Amritanshu21@users.noreply.github.com>
2023-10-14 00:15:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+gsap.from("#img1",{
+    opacity:0.1,
+   delay:2,
+   duration:4,
+   x:60
+    
+})
+gsap.to("#img1",{
+    opacity:0,
+    delay:5,
+    duration:2
+   
+})
+// gsap.from("#img2",{
+//     opacity:0,
+//     delay:7,
+//     duration:9
+// })
+// gsap.to("#img2",{
+//     opacity:1,
+   
+//  duration:4,
+//     x:-60
+// })
+// gsap.from("#img3",{
+//     opacity:0,
+//     delay:9,
+//     duration:12
+// })
+// gsap.to("#img3",{
+//     opacity:0,
+   
+//     duration:4,
+//     x:-60
+// })
+
+
+// gsap.from("#img2",{
+//     opacity:0,
+//        delay:5
+    
+// })
+// gsap.to("#img2",{
+//     opacity:1,
+//   duration:5,
+//   delay:4,
+//     x:-60
+// })
+// gsap.from("#img3",{
+//     opacity:0,
+//        delay:8
+    
+// })
+// gsap.to("#img3",{
+//     opacity:1,
+//   duration:5,
+//   delay:8,
+//     x:-60
+// })
+
+
+
+gsap.from("#img2",{
+    opacity:0.1,
+   delay:6,
+   duration:4,
+   x:60
+    
+})
+gsap.to("#img2",{
+    opacity:0,
+    delay:9,
+    duration:2
+   
+})
+gsap.from("#img3",{
+    delay:10,
+    opacity:0.1,
+
+  
+   duration:4,
+   x:50
+    
+})
+gsap.to("#img3",{
+    opacity:0,
+    delay:12,
+    duration:2
+   
+})
